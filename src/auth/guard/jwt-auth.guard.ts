4fcd1555fc176af1037abbfe7d12c1b92741b66a
ModellbahnFreak <freak.mdellbahn@gmail.com>
2020-06-27 21:01:18 +0200

    Clead unnecessary files

--- src/auth/guard/jwt-auth.guard.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-/**
- * Jwt auth guard class to prevent magic strings in JS code after compiling.
- */
-@Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') { }
